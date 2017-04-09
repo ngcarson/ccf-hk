6a21f601c98af3aac14a1197c17294b3a0394ef6
Carson Ng <ngcarson@gmail.com>
2017-04-09 21:13:52 +0800

    Revised javascript in countdown timer

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -1,5 +1,4 @@
-$(function () {
-  function getTimeRemaining(endtime) {
+function getTimeRemaining(endtime) {
     var t = Date.parse(endtime) - Date.parse(new Date());
     var seconds = Math.floor((t / 1000) % 60);
     var minutes = Math.floor((t / 1000 / 60) % 60);
@@ -41,4 +40,9 @@ $(function () {
   var deadline = 'April 16 2017 10:00:00 GMT+8';
   //var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
   initializeClock('clockdiv', deadline);
+
+/*  
+$(function () {
+
 });
+*/
