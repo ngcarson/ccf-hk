96a2e4867ca12d7309635a7886f7c126f95fbb1a
Carson Ng <ngcarson@gmail.com>
2017-04-11 02:25:56 +0800

    Corrected local time zone and added ccf logo in countdown section

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -1,21 +1,22 @@
 function myFunction() {
     var x = document.getElementById("myLocalDate").value;
     document.getElementById("demo").innerHTML = x;
+    //document.getElementById("demo").innerHTML = (x.replace('T',' ').replace('-','/'));
 
     function getTimeRemaining(endtime) {
-    var t = Date.parse(endtime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000 * 60 * 60 * 24));
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
+      var t = Date.parse(endtime) - Date.parse(new Date());
+      var seconds = Math.floor((t / 1000) % 60);
+      var minutes = Math.floor((t / 1000 / 60) % 60);
+      var hours = Math.floor((t / (1000 * 60 * 60)) % 24)-8;
+      var days = Math.floor(t / (1000 * 60 * 60 * 24));
+      return {
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+      };
+    }
 
   function initializeClock(id, endtime) {
     var clock = document.getElementById(id);
@@ -44,7 +45,7 @@ function myFunction() {
   var deadline = x;
   
   // Hard code specific datetime
-  //var deadline = 'April 16 2017 10:00:00 GMT+8';
+  //var deadline = 'April 11 2017 00:30:00 GMT+8';
   
   // Original sample code
   //var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
