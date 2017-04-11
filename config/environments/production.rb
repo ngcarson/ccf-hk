56acecdadb82fa053d3168e05d22ab29aa905c4c
Carson Ng <ngcarson@gmail.com>
2017-04-11 14:45:59 +0800

    Renamed assets to images in countdown.html.erb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # Set
+  # Sets 
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
