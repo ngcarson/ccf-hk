39a106c78041fd530ccc0d6a1a9ac9509ff265c8
Carson Ng <ngcarson@gmail.com>
2017-04-16 01:26:02 +0800

    Removed -8 from clock.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.datetimepicker.full.min.js
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -7,7 +7,7 @@ function myFunction() {
       var t = Date.parse(endtime) - Date.parse(new Date());
       var seconds = Math.floor((t / 1000) % 60);
       var minutes = Math.floor((t / 1000 / 60) % 60);
-      var hours = Math.floor((t / (1000 * 60 * 60)) % 24)-8;
+      var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
       var days = Math.floor(t / (1000 * 60 * 60 * 24));
       return {
         'total': t,
