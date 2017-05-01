33307503bf754fe3f1aba5d9a9f858c902af7b71
Carson Ng <ngcarson@gmail.com>
2017-05-02 00:39:16 +0800

    Setup forget password email

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,18 @@ module CcfTimer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+  
+  ActionMailer::Base.smtp_settings = {
+
+  	:address								=> 'smtp.gmail.com'
+  	:domain									=> 'mail.google.com',
+  	:port										=> 587,
+  	:user_name							=> "teamg426@gmail.com", #ENV['GMAIL_USERNAME'],
+  	:password								=> "FFheubsf3", #ENV['GMAIL_PASSWORD'],
+  	:authentication					=> 'login',
+  	:enable_starttls_auto		=> true
+
+  	}
+
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,6 @@ Rails.application.configure do
 
   # Required for Heroku
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  
 end
