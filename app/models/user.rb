dc5661d600da5212be15be817a518e2e8d6e6b97
Carson Ng <ngcarson@gmail.com>
2017-05-03 16:34:23 +0800

    Added surname, first_name, middle_name in account settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :surname, presence: true, length: { maximum: 20 }, format: { with:/\A[a-zA-Z_ ]+\Z/, message: ': Only letters are allowed' }
+
+  def self.human_attribute_name(attr, options = {})
+  	attr == :surname ? 'Surname' : super
+  end
+
+  validates :first_name, presence: true, length: { maximum: 20 }, format: { with:/\A[a-zA-Z_ ]+\Z/, message: ': Only letters are allowed' }
+  
+	def self.human_attribute_name(attr, options = {})
+  	attr == :first_name ? 'First Name' : super
+  end
+
+  validates :middle_name, presence: true, length: { maximum: 20 }, format: { with:/\A[a-zA-Z_ ]+\Z/, message: ': Only letters are allowed' }
+
+  def self.human_attribute_name(attr, options = {})
+  	attr == :middle_name ? 'Middle Name' : super
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425091056) do
+ActiveRecord::Schema.define(version: 20170503065940) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.string   "name"
+    t.string   "midle_name"
+    t.string   "surname"
+    t.string   "first_name"
+    t.string   "middle_name"
+    t.datetime "date_of_birth"
+    t.boolean  "is_female",              default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
